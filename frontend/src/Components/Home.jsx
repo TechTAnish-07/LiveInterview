e21b918ca7bbcb8061f9cd56cbf2728aef0c30c3
TechTAnish-07 <patidar29tanish@gmail.com>
2026-03-11 21:45:03 +0530

    home

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from './AuthProvider';
-import { motion } from "framer-motion"
 
 
 const Home = () => {
